cdcfcdfb0c7c452bbf6b4304a8335ebe799b72a3
flavio herrera <herreraflavio0@gmail.com>
2023-02-27 18:22:08 -0800

    changes to your code

--- main.cpp
+++ main.cpp
@@ -1,59 +1,114 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "checkArray.cpp"
-using namespace std;
 
+using namespace std;
+int checkArraySort(string *a, int array_size)
+{
+    int num = 0;
+
+    // Check if the array is in ascending order
+    bool ascending = true;
+    for (int i = 1; i < array_size; i++)
+    {
+        if (a[i] < a[i - 1])
+        {
+            ascending = false;
+            break;
+        }
+    }
+
+    // Cehck if the array is in descending order
+    bool descending = true;
+    for (int i = 1; i < array_size; i++)
+    {
+        if (a[i] > a[i - 1])
+        {
+            descending = false;
+            break;
+        }
+    }
+
+    if (ascending)
+    {
+        num = 1;
+    }
+    else if (descending)
+    {
+        num = -1;
+    }
+
+    return num;
+}
 int main()
 {
-    ifstream words_in;
-    ofstream words_out;
-    string line;
-    int size = 0;
-    words_in.open("words_in.txt");
-    words_out.open("words_out.txt");
-    while (!words_in.eof())
+
+    char fileName[] = "words_in.txt";
+
+    // Create an ifstream object
+    ifstream read;
+    read.open(fileName);
+
+    // Check if file exist
+    //  ! returns the opposite boolean value
+    if (!read)
     {
-        getline(words_in, line);
-        words_out << line << "\n";
-        size++;
+        cout << "File does not exist. Terminate the program." << endl;
+        return 0;
     }
-    string *stringArray = new string[size];
+    string word;
 
-    words_in.close();
+    int counter = 0;
 
-    // Re-open the input file
-
-    words_in.open("words_in.txt");
-    int i = 0;
-    while (!words_in.eof())
+    // read number of words in the file
+    while (!read.eof())
     {
-        getline(words_in, line);
-        stringArray[i] = line;
+        read >> word;
+        counter++;
+    }
+
+    // close read
+    read.close();
+
+    // Open the file to read words again
+    read.open(fileName);
+
+    // Create an ofstream object fout to write words to file "words_out.txt"
+    ofstream fout;
+
+    fout.open("words_out.txt");
+
+    string *stringwords = new string[counter];
+
+    int i = 0;
+    while (!read.eof())
+    {
+        read >> word;
+        stringwords[i] = word;
+
+        fout << word << endl;
         i++;
     }
 
-    // Print the words in the array
-    // for (int i = 0; i < size; i++)
-    // {
-    //     cout << stringArray[i] << endl;
-    // }
+    // close the ifstream and ofstream
+    read.close();
+    fout.close();
 
-    words_in.close();
-    words_out.close();
+    // Pause the program output on console
 
-    int checker = checkArraySort(stringArray, size);
+    int checker = checkArraySort(stringwords, counter);
     if (checker == 1)
     {
         cout << "The array is sorted in ascending order!" << endl;
     }
     else if (checker == -1)
     {
-        cout << "The array is sorted in descending order!" << endl;
+        cout << "This array is sorted in descending order!" << endl;
     }
     else
     {
         cout << "The array is not sorted!" << endl;
     }
+
     return 0;
 }
